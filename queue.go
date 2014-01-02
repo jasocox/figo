5a64ade5463cbbaa07b100f18933114a0fbd9010
Jason Cox <JasonWilburCox@gmail.com>
2014-01-02 10:20:41 -0500

    Made a separate thread safe queue

--- queue.go
+++ queue.go
@@ -14,24 +14,26 @@ type Queue interface {
 
 type queue struct {
   l *list.List
+}
+
+type aqueue struct {
+  q *queue
   lock sync.Mutex
 }
 
 func New() Queue {
-  return queue{l: list.New()}
+  return queue{list.New()}
+}
+
+func NewAsync() Queue {
+  return aqueue{q: &queue{list.New()}}
 }
 
 func (q queue) Push(o interface{}) {
-  q.lock.Lock()
-  defer q.lock.Unlock()
-
   q.l.PushBack(o)
 }
 
 func (q queue) Pop() interface{} {
-  q.lock.Lock()
-  defer q.lock.Unlock()
-
   e := q.l.Front()
   if e == nil {
     return nil
@@ -46,4 +48,26 @@ func (q queue) IsEmpty() bool {
 
 func (q queue) Len() int {
   return q.l.Len()
+}
+
+func (q aqueue) Push(o interface{}) {
+  q.lock.Lock()
+  defer q.lock.Unlock()
+
+  q.q.Push(o)
+}
+
+func (q aqueue) Pop() interface{} {
+  q.lock.Lock()
+  defer q.lock.Unlock()
+
+  return q.q.Pop()
+}
+
+func (q aqueue) IsEmpty() bool {
+  return q.q.IsEmpty()
+}
+
+func (q aqueue) Len() int {
+  return q.q.Len()
 }
--- queue_test.go
+++ queue_test.go
@@ -64,3 +64,66 @@ func Test_CanAddMultipleItems(t *testing.T) {
     }
   }
 }
+
+func TestAsync(t *testing.T) {
+  _ = NewAsync()
+}
+
+func TestAsync_CanPush(t *testing.T) {
+  q := NewAsync()
+  q.Push("Nada")
+}
+
+func TestAsync_CanPop(t *testing.T) {
+  q := NewAsync()
+  _ = q.Pop()
+}
+
+func TestAsync_CanIsEmpty(t *testing.T) {
+  q := NewAsync()
+  _ = q.IsEmpty()
+}
+
+func TestAsync_CanLen(t *testing.T) {
+  q := NewAsync()
+  _ = q.Len()
+}
+
+func TestAsync_CanAddOneItem(t *testing.T) {
+  q := NewAsync()
+  if q.Len() != 0 {
+    t.Error("Didn't get the right size")
+  }
+
+  q.Push("Nada")
+  if q.Len() != 1 {
+    t.Error("Should have increased in size")
+  }
+
+  val := q.Pop()
+  if val != "Nada" {
+    t.Error("Did not get the correct value")
+  }
+
+  if q.Len() != 0 {
+    t.Error("Should have decreased in size")
+  }
+}
+
+func TestAsync_CanAddMultipleItems(t *testing.T) {
+  q := NewAsync()
+
+  for i:=0; i<10; i++ {
+    q.Push(i)
+  }
+  if q.IsEmpty() {
+    t.Error("The queue should not be empty")
+  }
+
+  for i:=0; !q.IsEmpty(); i++ {
+    v := q.Pop()
+    if v != i {
+      t.Error("Did not get the correct value")
+    }
+  }
+}
