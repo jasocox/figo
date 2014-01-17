30d2352d16669003c305e730218eaed004c4d5da
Jason Cox <JasonWilburCox@gmail.com>
2014-01-17 09:26:12 -0500

    go fmt

--- queue.go
+++ queue.go
@@ -1,96 +1,96 @@
 package queue
 
 import (
-  "container/list"
-  "sync"
+	"container/list"
+	"sync"
 )
 
 type Queue interface {
-  Push(interface{})
-  Pop() interface{}
-  IsEmpty() bool
-  Len() int
+	Push(interface{})
+	Pop() interface{}
+	IsEmpty() bool
+	Len() int
 
-  Front() *list.Element
-  Next(*list.Element) *list.Element
+	Front() *list.Element
+	Next(*list.Element) *list.Element
 }
 
 type queue struct {
-  l *list.List
+	l *list.List
 }
 
 type aqueue struct {
-  q *queue
-  lock sync.Mutex
+	q    *queue
+	lock sync.Mutex
 }
 
 func New() Queue {
-  return queue{list.New()}
+	return queue{list.New()}
 }
 
 func NewSync() Queue {
-  return aqueue{q: &queue{list.New()}}
+	return aqueue{q: &queue{list.New()}}
 }
 
 func (q queue) Push(o interface{}) {
-  q.l.PushBack(o)
+	q.l.PushBack(o)
 }
 
 func (q queue) Pop() interface{} {
-  e := q.l.Front()
-  if e == nil {
-    return nil
-  }
+	e := q.l.Front()
+	if e == nil {
+		return nil
+	}
 
-  return q.l.Remove(e)
+	return q.l.Remove(e)
 }
 
 func (q queue) IsEmpty() bool {
-  return q.l.Len() == 0
+	return q.l.Len() == 0
 }
 
 func (q queue) Len() int {
-  return q.l.Len()
+	return q.l.Len()
 }
 
 func (q queue) Front() *list.Element {
-  return q.l.Front()
+	return q.l.Front()
 }
 
 func (q queue) Next(e *list.Element) *list.Element {
-  if e == nil {
-    return e
-  }
+	if e == nil {
+		return e
+	}
 
-  return e.Next()
+	return e.Next()
 }
 
 func (q aqueue) Push(o interface{}) {
-  q.lock.Lock()
-  defer q.lock.Unlock()
+	q.lock.Lock()
+	defer q.lock.Unlock()
 
-  q.q.Push(o)
+	q.q.Push(o)
 }
 
 func (q aqueue) Pop() interface{} {
-  q.lock.Lock()
-  defer q.lock.Unlock()
+	q.lock.Lock()
+	defer q.lock.Unlock()
 
-  return q.q.Pop()
+	return q.q.Pop()
 }
 
 func (q aqueue) IsEmpty() bool {
-  return q.q.IsEmpty()
+	return q.q.IsEmpty()
 }
 
 func (q aqueue) Len() int {
-  return q.q.Len()
+	return q.q.Len()
 }
 
 func (q aqueue) Front() *list.Element {
-  return q.q.Front()
+	return q.q.Front()
 }
 
 func (q aqueue) Next(e *list.Element) *list.Element {
-  return q.q.Next(e)
+	return q.q.Next(e)
 }
--- queue_test.go
+++ queue_test.go
@@ -3,162 +3,163 @@ package queue
 import "testing"
 
 func TestNew(t *testing.T) {
-  _ = New()
+	_ = New()
 }
 
 func Test_CanPush(t *testing.T) {
-  q := New()
-  q.Push("Nada")
+	q := New()
+	q.Push("Nada")
 }
 
 func Test_CanPop(t *testing.T) {
-  q := New()
-  _ = q.Pop()
+	q := New()
+	_ = q.Pop()
 }
 
 func Test_CanIsEmpty(t *testing.T) {
-  q := New()
-  _ = q.IsEmpty()
+	q := New()
+	_ = q.IsEmpty()
 }
 
 func Test_CanLen(t *testing.T) {
-  q := New()
-  _ = q.Len()
+	q := New()
+	_ = q.Len()
 }
 
 func Test_CanAddOneItem(t *testing.T) {
-  q := New()
-  if q.Len() != 0 {
-    t.Error("Didn't get the right size")
-  }
+	q := New()
+	if q.Len() != 0 {
+		t.Error("Didn't get the right size")
+	}
 
-  q.Push("Nada")
-  if q.Len() != 1 {
-    t.Error("Should have increased in size")
-  }
+	q.Push("Nada")
+	if q.Len() != 1 {
+		t.Error("Should have increased in size")
+	}
 
-  val := q.Pop()
-  if val != "Nada" {
-    t.Error("Did not get the correct value")
-  }
+	val := q.Pop()
+	if val != "Nada" {
+		t.Error("Did not get the correct value")
+	}
 
-  if q.Len() != 0 {
-    t.Error("Should have decreased in size")
-  }
+	if q.Len() != 0 {
+		t.Error("Should have decreased in size")
+	}
 }
 
 func Test_CanAddMultipleItems(t *testing.T) {
-  q := New()
+	q := New()
 
-  for i:=0; i<10; i++ {
-    q.Push(i)
-  }
-  if q.IsEmpty() {
-    t.Error("The queue should not be empty")
-  }
+	for i := 0; i < 10; i++ {
+		q.Push(i)
+	}
+	if q.IsEmpty() {
+		t.Error("The queue should not be empty")
+	}
 
-  for i:=0; !q.IsEmpty(); i++ {
-    v := q.Pop()
-    if v != i {
-      t.Error("Did not get the correct value")
-    }
-  }
+	for i := 0; !q.IsEmpty(); i++ {
+		v := q.Pop()
+		if v != i {
+			t.Error("Did not get the correct value")
+		}
+	}
 }
 
 func Test_CanIterateEmptyList(t *testing.T) {
-  q := New()
+	q := New()
 
-  i := 0
-  for elem:=q.Front(); q.Next(elem)!=nil; elem=q.Next(elem) { }
+	i := 0
+	for elem := q.Front(); q.Next(elem) != nil; elem = q.Next(elem) {
+	}
 
-  if i != 0 {
-    t.Error("Should have been empty")
-    return
-  }
+	if i != 0 {
+		t.Error("Should have been empty")
+		return
+	}
 }
 
 func Test_CanIterateItems(t *testing.T) {
-  q := New()
+	q := New()
 
-  for i:=0; i<10; i++ {
-    q.Push(i)
-  }
+	for i := 0; i < 10; i++ {
+		q.Push(i)
+	}
 
-  i := 0
-  for elem:=q.Front(); q.Next(elem)!=nil; elem=q.Next(elem) {
-    if elem.Value.(int) != i {
-      t.Error("Did not match the correct order")
-      return
-    }
+	i := 0
+	for elem := q.Front(); q.Next(elem) != nil; elem = q.Next(elem) {
+		if elem.Value.(int) != i {
+			t.Error("Did not match the correct order")
+			return
+		}
 
-    i++
-  }
+		i++
+	}
 
-  if i != 9 {
-    t.Error("Did not iterate the whole list")
-    return
-  }
+	if i != 9 {
+		t.Error("Did not iterate the whole list")
+		return
+	}
 }
 
 func TestSync(t *testing.T) {
-  _ = NewSync()
+	_ = NewSync()
 }
 
 func TestSync_CanPush(t *testing.T) {
-  q := NewSync()
-  q.Push("Nada")
+	q := NewSync()
+	q.Push("Nada")
 }
 
 func TestSync_CanPop(t *testing.T) {
-  q := NewSync()
-  _ = q.Pop()
+	q := NewSync()
+	_ = q.Pop()
 }
 
 func TestSync_CanIsEmpty(t *testing.T) {
-  q := NewSync()
-  _ = q.IsEmpty()
+	q := NewSync()
+	_ = q.IsEmpty()
 }
 
 func TestSync_CanLen(t *testing.T) {
-  q := NewSync()
-  _ = q.Len()
+	q := NewSync()
+	_ = q.Len()
 }
 
 func TestSync_CanAddOneItem(t *testing.T) {
-  q := NewSync()
-  if q.Len() != 0 {
-    t.Error("Didn't get the right size")
-  }
+	q := NewSync()
+	if q.Len() != 0 {
+		t.Error("Didn't get the right size")
+	}
 
-  q.Push("Nada")
-  if q.Len() != 1 {
-    t.Error("Should have increased in size")
-  }
+	q.Push("Nada")
+	if q.Len() != 1 {
+		t.Error("Should have increased in size")
+	}
 
-  val := q.Pop()
-  if val != "Nada" {
-    t.Error("Did not get the correct value")
-  }
+	val := q.Pop()
+	if val != "Nada" {
+		t.Error("Did not get the correct value")
+	}
 
-  if q.Len() != 0 {
-    t.Error("Should have decreased in size")
-  }
+	if q.Len() != 0 {
+		t.Error("Should have decreased in size")
+	}
 }
 
 func TestSync_CanAddMultipleItems(t *testing.T) {
-  q := NewSync()
+	q := NewSync()
 
-  for i:=0; i<10; i++ {
-    q.Push(i)
-  }
-  if q.IsEmpty() {
-    t.Error("The queue should not be empty")
-  }
+	for i := 0; i < 10; i++ {
+		q.Push(i)
+	}
+	if q.IsEmpty() {
+		t.Error("The queue should not be empty")
+	}
 
-  for i:=0; !q.IsEmpty(); i++ {
-    v := q.Pop()
-    if v != i {
-      t.Error("Did not get the correct value")
-    }
-  }
+	for i := 0; !q.IsEmpty(); i++ {
+		v := q.Pop()
+		if v != i {
+			t.Error("Did not get the correct value")
+		}
+	}
 }
