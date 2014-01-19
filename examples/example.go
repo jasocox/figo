88edfa80c31a3034a11f9c341207f32f3e7764c1
Jason Cox <JasonWilburCox@gmail.com>
2014-01-18 20:42:57 -0500

    Adding an example

---
+++ examples/example.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+  "github.com/jasocox/queue"
+  "fmt"
+)
+
+const SIZE=10
+
+func main() {
+  q := queue.New()
+
+  fmt.Printf("Is empty: %t\n", q.IsEmpty())
+
+  for i:=0; i<SIZE; i++ {
+    q.Push(i)
+  }
+
+  fmt.Println("Iterating")
+  for elem:=q.Front(); elem!=nil; elem=q.Next(elem) {
+    fmt.Printf("%d ", elem.Value)
+  }
+  fmt.Println()
+
+  fmt.Printf("Is empty: %t\n", q.IsEmpty())
+
+  fmt.Println("Popping")
+  for val:=q.Pop(); val!=nil; val=q.Pop() {
+    fmt.Printf("%d ", val)
+  }
+  fmt.Println()
+
+  fmt.Printf("Is empty: %t\n", q.IsEmpty())
+}
