73b1364ff227580c24fb9b60a3f11736dc4a7ac0
Jason Cox <JasonWilburCox@gmail.com>
2014-01-20 07:54:23 -0500

    go fmt

--- examples/example.go
+++ examples/example.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-  "github.com/jasocox/queue"
-  "fmt"
+	"fmt"
+	"github.com/jasocox/queue"
 )
 
-const SIZE=10
+const SIZE = 10
 
 func main() {
-  q := queue.New()
+	q := queue.New()
 
-  fmt.Printf("Is empty: %t\n", q.IsEmpty())
+	fmt.Printf("Is empty: %t\n", q.IsEmpty())
 
-  for i:=0; i<SIZE; i++ {
-    q.Push(i)
-  }
+	for i := 0; i < SIZE; i++ {
+		q.Push(i)
+	}
 
-  fmt.Println("Iterating")
-  for elem:=q.Front(); elem!=nil; elem=q.Next(elem) {
-    fmt.Printf("%d ", elem.Value)
-  }
-  fmt.Println()
+	fmt.Println("Iterating")
+	for elem := q.Front(); elem != nil; elem = q.Next(elem) {
+		fmt.Printf("%d ", elem.Value)
+	}
+	fmt.Println()
 
-  fmt.Printf("Is empty: %t\n", q.IsEmpty())
+	fmt.Printf("Is empty: %t\n", q.IsEmpty())
 
-  fmt.Println("Popping")
-  for val:=q.Pop(); val!=nil; val=q.Pop() {
-    fmt.Printf("%d ", val)
-  }
-  fmt.Println()
+	fmt.Println("Popping")
+	for val := q.Pop(); val != nil; val = q.Pop() {
+		fmt.Printf("%d ", val)
+	}
+	fmt.Println()
 
-  fmt.Printf("Is empty: %t\n", q.IsEmpty())
+	fmt.Printf("Is empty: %t\n", q.IsEmpty())
 }
